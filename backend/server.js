85500b444ff56e05c379723ada5298f9d716691c
Aditya Shrivas <as958622@gmail.com>
2026-06-29 23:19:18 +0530

    all changes done

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express()
 
 // app.use(cors())
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://erp-task-42s8.vercel.app',
   credentials: true
 }))
 app.use(express.json())
